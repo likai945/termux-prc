8a2117bb001249946d764cb0c3a79e4a8562825f
LiKai <likai945@foxmail.com>
2021-06-24 19:19:37 +0800

    2021Jun24•19:19:37

--- hh.sh
+++ hh.sh
@@ -2,6 +2,5 @@
 while [ 1 ]
 do
 	n=`date +%M`
-	[ $n == "00" -o $n == "30" ] && echo -e '\a' && read -p "acted on `date +%H:%M`? " a
-	sleep 61
+	[ $n == "00" -o $n == "30" ] && echo -e '\a' && echo -n "acted on `date +%H:%M`? " && sleep 61
 done
