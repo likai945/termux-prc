c2c0e5b51317fae9c4acf3efbe1d729462a70c18
LiKai <likai945@foxmail.com>
2021-07-24 12:11:29 +0800

    cf_molecule2021Jul24•12:11:29

--- chck_file_molecule.sh
+++ chck_file_molecule.sh
@@ -2,10 +2,12 @@
 #####check if the pics not shotted this day#####
 listNotThisDayShots(){
 	rq=`ls -lR | grep -E '\.sh' | awk '{print $6,$7}' | sort | uniq | wc -l`
-	[ $rq -gt 1 ] && \
-	echo -e '\e[42mScreenshots not this day:\e[0m' && \
-	ls -lR | grep '\.sh' | grep -v "`date "+%b %e"`" | awk '{print $NF}' || \
-	echo -e "\e[44mall pictures screenshotted this day.\e[0m"
+	if [ $rq -gt 1 ];then
+		echo -e '\e[42mScreenshots not this day:\e[0m'
+		ls -lR | grep '\.sh' | grep -v "`date "+%b %e"`" | awk '{print $NF}'
+	else
+		echo -e "\e[44mall pictures screenshotted this day.\e[0m"
+	fi
 }
 #####check if there are any empty directories#####
 listEmptyDir(){
