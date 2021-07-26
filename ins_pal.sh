8832e9662ab37656d217994f1ade7644176c77ab
LiKai <likai945@foxmail.com>
2021-07-26 17:35:08 +0800

    ins_pal.sh_fixed_bug2021Jul26•17:35:08

--- ins_pal.sh
+++ ins_pal.sh
@@ -29,12 +29,18 @@ show(){
 }
 #####check if the pics not shotted this day#####
 listNotThisDayShots(){
-	rq=`ls -lR | grep -E '\.sh' | awk '{print $6,$7}' | sort | uniq | wc -l`
+	rq=`ls -lR | grep -E '.jpg|.png' | awk '{print $7,$8}'  | sort | uniq | wc -l`
 	if [ $rq -gt 1 ];then
 		echo -e '\e[42mScreenshots not this day:\e[0m'
-		ls -lR | grep '\.sh' | grep -v "`date "+%b %e"`" | awk '{print $NF}'
+		ls -lR | grep -E '.jpg|.png' | grep -v "`date "+%b %e"`" | awk '{print $NF}'
 	else
-		echo -e "\e[44mall pictures screenshotted this day.\e[0m"
+		ls -lR | grep -E '.jpg|.png' | grep -q "`date "+%b %e"`" 
+		if [ $? -eq 0 ];then
+			echo -e "\e[44mall pictures screenshotted this day.\e[0m"
+		else
+			echo -e '\e[42mScreenshots not this day:\e[0m'
+			ls -lR | grep -E '.jpg|.png' | grep -v "`date "+%b %e"`" | awk '{print $NF}' 
+		fi
 	fi
 }
 #####check if there are any empty directories#####
@@ -42,7 +48,7 @@ listEmptyDir(){
 	mn=`ls -R | sed -n '/:/,+1p' | sed -n '/^$/{x;p};h' | wc -l`
 	en=`ls -R | sed -n '/:/,+1p' | tail -1 | grep ':' | wc -l`
 	if [ $[mn+en] -eq 0 ];then
-		echo -e '\e[44mno mempty directories:\e[0m' 
+		echo -e '\e[44mno empty directories.\e[0m' 
 	else
 		echo -e '\e[42mempty directories:\e[0m' 
 		ls -R | sed -n '/:/,+1p' | sed -n '/^$/{x;p};h'
