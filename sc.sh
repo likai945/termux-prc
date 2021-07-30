2b0f89bed953d3062e2af9176c0ccc32ab4f5b53
LiKai <likai945@foxmail.com>
2021-07-31 00:32:51 +0800

    sc.sh2021Jul31•00:32:51

---
+++ sc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+line(){
+	for i in $(seq `echo "$1" | wc -L`)
+	do
+		echo -en '\e[33m-\e[0m'
+	done
+		echo
+}
+check(){
+	while [ 1 ]
+	do
+		read -p "screenshot ${1}?" e
+		if [ ${e:="yes"} == "yes" ];then
+			cd $1
+			line `pwd`
+			pwd
+			line `pwd`
+			ls
+			break
+		elif [ $e == "no" ];then
+			break
+		else
+			echo "yes or no"
+		fi
+	done
+}
+####the program####
+check .
+check ..
