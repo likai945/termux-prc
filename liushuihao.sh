6eefaf992ce51627e52cdf1ecadb49cd3f8cb75a
LiKai <likai945@foxmail.com>
2021-08-24 22:26:20 +0800

    liushuihao.sh_rewrite2021Aug24•22:26:20

--- liushuihao.sh
+++ liushuihao.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-sc=`cat shangci`
-today=`date +%j`
-[ -e $sc ] && [ $sc -ne $today ] && rm $sc && echo 1 > $today && echo $today > shangci
-i=`cat $today`
-echo -e "\t\t$i" && let i++ && echo $i > $today
+sc=23
+jt=`date +%j`
+i=6
+count(){
+	while [ 1 ];do
+		read -p "count? " c
+		if [[ ${c:="yes"} == "yes" ]];then
+			echo -e "\t$i"
+			let i++
+			sed -i "/^i=/s/^.*$/i=$i/" $0
+		elif [[ "$c" =~ "quit" ]];then
+			exit
+		fi
+	done
+}
+
+######the main######
+
+if [ "$jt" != "$sc" ];then
+	i=1
+	count
+	sed -i "/^sc=/s/^.*$/sc=$jt/" $0
+else
+	count
+fi
