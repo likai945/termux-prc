70706eebe9b619a2547d421df2b8e457b6eff384
LiKai <likai945@foxmail.com>
2021-11-14 00:32:22 +0800

    leazyacc72021Nov14•00:32:22

--- leazyacc7_auto.py
+++ leazyacc7_auto.py
@@ -101,44 +101,44 @@ def write_contents(sheet,hisorcrt):
             if hisorcrt=='crt':
                 line[1]=now
                 line=line+['是','']
-            sheet.write_row(r,2,line,cfmt)
+            sheet.write_row(r,2,line,bfmt)
             r+=1
         rs.append(r)
     return rs
 
 def write_lframe(sheet,rows):
-    sheet.merge_range(2,0,rows[2]-1,0,date,lfmt)
-    sheet.merge_range(2,1,rows[0]-1,1,'可信3',lfmt)
-    sheet.merge_range(rows[0],1,rows[1]-1,1,'可信4',lfmt)
-    sheet.merge_range(rows[1],1,rows[2]-1,1,'可信5',lfmt)
+    sheet.merge_range(2,0,rows[2]-1,0,date,cfmt)
+    sheet.merge_range(2,1,rows[0]-1,1,'可信3',cfmt)
+    sheet.merge_range(rows[0],1,rows[1]-1,1,'可信4',cfmt)
+    sheet.merge_range(rows[1],1,rows[2]-1,1,'可信5',cfmt)
 
 
 def write_his_sheet(sheet):
-    sheet.merge_range(0,0,0,7,'历史告警',lfmt)
+    sheet.merge_range(0,0,0,7,'历史告警',cfmt)
     title=['时间','资源池','告警时间','清除时间','对象名称','告警描述','数量','处理结果']
-    sheet.write_row(1,0,title,cfmts)
+    sheet.write_row(1,0,title,cfmt)
     hisrows=write_contents(sheet,'his')
     write_lframe(sheet,hisrows)
 
 
 def write_crt_sheet(sheet):
-    sheet.merge_range(0,0,0,9,'当前告警',lfmt)
+    sheet.merge_range(0,0,0,9,'当前告警',cfmt)
     title=['时间','资源池','告警时间','确认恢复时间','对象名称',' 告警描述','数量','处理结果','是否清除','未确认恢复原因']
-    sheet.write_row(1,0,title,cfmts)
+    sheet.write_row(1,0,title,cfmt)
     crtrows=write_contents(sheet,'crt')
     write_lframe(sheet,crtrows)
 
 
 def write_smr_sheet(sheet):
     title=['时间','资源池','历史告警数量','当前告警数量','当前告警清除数量','当前告警剩余']
-    sheet.write_row(0,0,title,cfmts)
-    sheet.merge_range(1,0,3,0,date,lfmt)
+    sheet.write_row(0,0,title,cfmt)
+    sheet.merge_range(1,0,3,0,date,cfmt)
     linekx3=['可信三',htotal3,ctotal3,ctotal3,0]
     linekx4=['可信四',htotal4,ctotal4,ctotal4,0]
     linekx5=['可信五',htotal5,ctotal5,ctotal5,0]
-    sheet.write_row(1,1,linekx3,cfmts)
-    sheet.write_row(2,1,linekx4,cfmts)
-    sheet.write_row(3,1,linekx5,cfmts)
+    sheet.write_row(1,1,linekx3,cfmt)
+    sheet.write_row(2,1,linekx4,cfmt)
+    sheet.write_row(3,1,linekx5,cfmt)
 
 def check_files_exist():
     for i in files:
@@ -163,18 +163,19 @@ def delete_files():
 today=time.localtime()
 date=f'{today[0]}-{today[1]}-{today[2]}'
 now=f'{date} {today[3]}:{today[4]}:{today[5]}'
+
 his3,his4,his5='kx3_h.csv','kx4_h.csv','kx5_h.csv'
 crt3,crt4,crt5='kx3_c.csv','kx4_c.csv','kx5_c.csv'
 files=[his3,his4,his5,crt3,crt4,crt5]
+
 bookname=f'附件7：资源池告警处理情况表-中兴资源池{date}.xlsx'
 book=xw.Workbook(bookname)
 sheetsmr=book.add_worksheet('汇总')
 sheethis=book.add_worksheet('历史告警处理记录')
 sheetcrt=book.add_worksheet('当前告警处理记录')
 
-lfmt=book.add_format({'align':'center','valign':'vcenter','border':1})
-cfmts=book.add_format({'align':'center','valign':'vcenter','border':1})
-cfmt=book.add_format({'border':1})
+cfmt=book.add_format({'align':'center','valign':'vcenter','border':1})
+bfmt=book.add_format({'border':1})
 
 if __name__=='__main__':
     check_files_exist()
