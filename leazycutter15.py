14ff1ce8c966d4062adb3f415b894cc9d3e21a83
LiKai <likai945@foxmail.com>
2022-05-20 18:16:54 +0800

    leazycutter15.py•2022May20•18:16:54

---
+++ leazycutter15.py
@@ -0,0 +1,218 @@
+# beta15
+# 针对不同资源池虚机存在相同识别码的处理
+# 用法：将导出的资料分别按照kx3.csv、kx41.csv、kx42.csv、kx5.csv命名，放置在本工具所在目录后双击稍事等待即可得到名为jzfz.csv的文件，由四列构成，分别为网元名、对应系统均值、均值、峰值和网元的虚机数量，与需填报的报表相对应。
+# 需配合dy.csv文件使用
+# by Li Kai
+
+
+import os
+
+
+def zhaodao_vnfname(vmn):
+    rpn = vmn.split('-')[3][1]
+    if "-OMC-" in vmn:
+        sbm = "OMC"
+    elif "-NFVO-" in vmn:
+        sbm = "NFVO"
+    elif "-xnqVNFM3" in vmn:
+        sbm = "VNFM03"
+    elif "-VNFM-" in vmn:
+        sbm = "VNFM"
+    elif "ZTE_EMSplus_RPT" in vmn:
+        sbm = "ZTE_EMSplus_RPT"
+    else:
+        vmlst = list(vmn.split('-'))
+        vmind = vmlst.index('VM')
+        if vmind == len(vmlst)-1:
+            psbm = 'noSBM'
+            man.add(vmn)
+        else:
+            psbm = vmlst[vmind+1]
+        if "ZX" in psbm:
+            sbm = psbm[:psbm.index("ZX") + 2]
+        else:
+            sbm = psbm
+    sbm = rpn + sbm
+    vnfname = dy.get(sbm, 'novnf')
+    return vnfname
+
+
+def hebing_wendang(jia, yi, qz):
+    if os.path.exists(yi):
+        if os.path.exists(jia):
+            with open(jia, "a", encoding='utf_8_sig') as jiawen:
+                q = qz
+                with open(yi, encoding='utf_8_sig') as yiwen:
+                    for i in yiwen:
+                        q += 1
+                        if q > 1:
+                            jiawen.write(str(i.rstrip()) + "\n")
+        else:
+            os.rename(yi, jia)
+
+
+def shan_wendang(*wendang):
+    for wd in wendang:
+        if os.path.exists(wd):
+            os.remove(wd)
+
+
+def bianli(chu, dxweizhi):
+    wydct = {}
+    with open(chu, encoding='utf_8_sig') as nwen:
+        k = 0
+        for line in nwen:
+            k += 1
+            if k > 1:
+                vmn = line.split(",")[3].rstrip()
+                vnf = zhaodao_vnfname(vmn).rstrip()
+                duixiang = line.split(",")[dxweizhi]
+                dxzhi = line.split(",")[4]
+                wydct[vnf] = wydct.get(vnf, {})
+                wydct[vnf][duixiang] = wydct[vnf].get(duixiang, [])
+                wydct[vnf][duixiang].append(float(dxzhi.strip('%\n')))
+    if 'novnf' in wydct:
+        del wydct['novnf']
+    return wydct
+
+
+def junzhisl(dct):
+    fengzhilb = []
+    for item in dct:
+        vmlbs = dct[item]
+        yuansu = max(vmlbs)
+        fengzhilb.append(yuansu)
+    jzjg = sum(fengzhilb) / len(fengzhilb)
+    jzjg = round(jzjg,2)
+    sljg = len(dct)
+    jieguo = f'{jzjg},{sljg}'
+    return jieguo
+
+
+def fengzhixt(dct):
+    junzhilb = []
+    for item in dct:
+        sjlbs = dct[item]
+        yuansu = sum(sjlbs) / len(sjlbs)
+        junzhilb.append(yuansu)
+    fzjg = max(junzhilb)
+    fzjg = round(fzjg,2)
+    xtjg = sum(junzhilb) / len(junzhilb)
+    xtjg = round(xtjg,2)
+    jieguo = f'{fzjg},{xtjg}'
+    return jieguo
+
+
+def vmjzfz(dct):
+    jieguo = []
+    for vm in dct:
+        vmavg = sum(dct[vm]) / len(dct[vm])
+        vmavg = round(vmavg,2)
+        vmmax = max(dct[vm])
+        vmavg = str(vmavg)
+        vmmax = str(vmmax)
+        yihang = f'{vm},,{vmavg},{vmmax}'
+        jieguo.append(yihang)
+    return jieguo
+
+
+def chuli_jzfz_wendang(jia,dct,func):
+    with open(jia, "a", encoding='utf_8_sig') as wen:
+        for vnf in dct:
+            jg = func(dct[vnf])
+            wen.write(vnf + ',' + str(jg) + '\n')
+
+
+def chuli_vmjzfz_wendang(yi, dct):
+    with open(yi, "a", encoding='utf_8_sig') as yiwen:
+        title='大区,网元唯一标识,虚拟机名称,虚拟机IP,CPU均值利用率（%）,CPU峰值利用率（%）\n'
+        yiwen.write(title)
+        for vnf in dct:
+            for line in vmjzfz(dct[vnf]):
+                yiwen.write(f'西南大区,{vnf},{line}\n')
+
+
+def chuli_wendang(chu, jia, yi, dxwz, func):
+    if os.path.exists(chu):
+        dct = bianli(chu, dxwz)
+        chuli_jzfz_wendang(jia,dct,func)
+        if func is junzhisl:
+            chuli_vmjzfz_wendang(yi, dct)
+
+
+def zidian(chu, lstweizhi, fstweizhi):
+    shengchengzd = {}
+    with open(chu, encoding='utf_8_sig') as nwen:
+        for i in nwen:
+            lst = i.split(",")[lstweizhi]
+            fst = i.split(",")[fstweizhi]
+            shengchengzd[fst] = lst
+    return shengchengzd
+
+
+def shengcheng_yingshe(chu, zhong):
+    if os.path.exists(chu):
+        with open(zhong, "a", encoding='utf_8_sig') as wen:
+            title = '网元,系统均值,均值,峰值,虚机数量\n'
+            wen.write(title)
+            sczdjz = zidian("jzsl.csv", 1, 0)
+            sczdfz = zidian("fzxt.csv", 1, 0)
+            sczdxt = zidian("fzxt.csv", -1, 0)
+            sczdsl = zidian("jzsl.csv", -1, 0)
+            with open(chu, encoding='utf_8_sig') as nwen:
+                for i in nwen:
+                    vnfname = i.split(",")[0].rstrip()
+                    xtjunzhi = sczdxt[vnfname].rstrip()
+                    junzhi = sczdjz[vnfname].rstrip()
+                    fengzhi = sczdfz[vnfname].rstrip()
+                    shuliang = sczdsl[vnfname].rstrip()
+                    line = f'{vnfname},{xtjunzhi},{junzhi},{fengzhi},{shuliang}'
+                    wen.write(str(line) + "\n")
+
+
+def check_dy_exists():
+    if os.path.exists("dy.csv"):
+        global dy
+        dy = zidian("dy.csv", -1, 0)
+    else:
+        print("File dy.csv does not exist!")
+        input("\nPress Enter to quit.")
+        exit(1)
+
+
+def main():
+    shan_wendang("jzfz.csv", "jzsl.csv", "fzxt.csv", "vmjzfz.csv")
+    print("Old files removed yet.")
+
+    hebing_wendang("kx41.csv", "kx42.csv", 0)
+    hebing_wendang("kx3.csv", "kx41.csv", 0)
+    hebing_wendang("kx3.csv", "kx5.csv", 0)
+
+    chuli_wendang('kx3.csv', 'jzsl.csv', 'vmjzfz.csv', 3, junzhisl)
+    print("Average and number done.")
+    print("AvgVM and MaxVM done.")
+    chuli_wendang('kx3.csv', 'fzxt.csv', None, 0, fengzhixt)
+    print("Max and sysavg done.")
+
+    shengcheng_yingshe("jzsl.csv", "jzfz.csv")
+    print("Done!")
+
+    shan_wendang("kx3.csv", "kx41.csv", "kx42.csv", "kx5.csv", "jzsl.csv", "fzxt.csv")
+
+
+def extra():
+    if man:
+        print('\nCheck VMs below:')
+        for i in man:
+            print(f'• {i}')
+        print('\nPress <ENTER> to quit.')
+        input()
+
+
+man=set()
+
+
+if __name__ == '__main__':
+    check_dy_exists()
+    main()
+    extra()
--- leazytable_line.py
+++ leazytable_line.py
@@ -8,12 +8,11 @@ import os
 import re
 
 
-
-def merge_files(one,two,three,cnt):
+def merge_files(one, two, three, cnt):
     with open(three, "a", encoding='utf_8_sig') as nf:
-        for fl in [one,two]:
+        for fl in [one, two]:
             with open(fl, encoding='utf_8_sig') as f:
-                tmp=cnt
+                tmp = cnt
                 for i in f:
                     tmp += 1
                     if tmp > 1 and not 'VM,' in i:
@@ -21,18 +20,19 @@ def merge_files(one,two,three,cnt):
 
 
 def init_files():
-#    merge_files('srvkx41m.csv','srvkx42m.csv','srvkx4m.csv',0)
-#    merge_files('srvkx41e.csv','srvkx42e.csv','srvkx4e.csv',0)
-    merge_files('srvkx4m.csv','srvkx4e.csv','srvkx4.csv',0)
-    for i in ['srv','vm']:
-        for j in range(3,6,2):
-            merge_files(f'{i}kx{j}m.csv',f'{i}kx{j}e.csv',f'{i}kx{j}.csv',0)
+    #    merge_files('srvkx41m.csv','srvkx42m.csv','srvkx4m.csv',0)
+    #    merge_files('srvkx41e.csv','srvkx42e.csv','srvkx4e.csv',0)
+    merge_files('srvkx4m.csv', 'srvkx4e.csv', 'srvkx4.csv', 0)
+    for i in ['srv', 'vm']:
+        for j in range(3, 6, 2):
+            merge_files(f'{i}kx{j}m.csv', f'{i}kx{j}e.csv', f'{i}kx{j}.csv', 0)
+
 
 def check_event(hint):
     while True:
-        num=input(hint)
+        num = input(hint)
         try:
-            num=int(num)
+            num = int(num)
             if num % 2 == 1:
                 print('\t\athis is an odd number.')
             elif num < 0:
@@ -41,144 +41,144 @@ def check_event(hint):
                 return num
         except ValueError:
             print('\t\athe input number may be wrong, have a check.')
-    
+
 
 def read_numbers():
-    cntnts=[]
-    for i in range(3,6):
-        cntnt=['西南大区',f'可信{i}资源池']
-        words=['total','used']
+    cntnts = []
+    for i in range(3, 6):
+        cntnt = ['西南大区', f'可信{i}资源池']
+        words = ['total', 'used']
         for j in range(2):
-            hint=f'srvkx{i} {words[j]} vCPUs: '
-            num=check_event(hint)
-            cntnt.append(num/2)
+            hint = f'srvkx{i} {words[j]} vCPUs: '
+            num = check_event(hint)
+            cntnt.append(num / 2)
         cntnt.append('')
         cntnt.append(cntnt[-2])
-        rate=cntnt[-1] / cntnt[2] * 100
+        rate = cntnt[-1] / cntnt[2] * 100
         cntnt.append(rate)
         cntnts.append(cntnt)
     return cntnts
 
 
 def write_sheet_cmp_rsc():
-    title=['大区','资源池','总可分配物理核','实际已分配物理核','施工图设计会审的预分配物理核','各类虚拟化应用分配物理核总量','计算资源分配率（%']
-    sheetCmpRsc.write_row(0,0,title,cfmt)
-    cntnts=read_numbers()
-    row=0
+    title = ['大区', '资源池', '总可分配物理核', '实际已分配物理核', '施工图设计会审的预分配物理核', '各类虚拟化应用分配物理核总量', '计算资源分配率（%']
+    sheetCmpRsc.write_row(0, 0, title, cfmt)
+    cntnts = read_numbers()
+    row = 0
     for line in cntnts:
-        row+=1
-        sheetCmpRsc.write_row(row,0,line,cfmt)
+        row += 1
+        sheetCmpRsc.write_row(row, 0, line, cfmt)
 
 
 def walk_the_file(file):
     with open(file) as f:
-        fl=csv.reader(f)
-        dct={}
+        fl = csv.reader(f)
+        dct = {}
         for line in fl:
-            vmn=line[-2]
-            val=line[-1].rstrip('%\n')
-            val=float(val)
-            val=val if val >= 0 else 0
-            dct.setdefault(vmn,[])
+            vmn = line[-2]
+            val = line[-1].rstrip('%\n')
+            val = float(val)
+            val = val if val >= 0 else 0
+            dct.setdefault(vmn, [])
             dct[vmn].append(val)
         return dct
 
-            
+
 def fmt_the_dct(file):
-    dct=walk_the_file(file)
-    fmtDct={}
-    lst=list(dct.keys())
+    dct = walk_the_file(file)
+    fmtDct = {}
+    lst = list(dct.keys())
     lst.sort()
     for k in dct:
-        valLst=dct[k]
-        avg=sum(valLst)/len(valLst)
-        fmtDct[k]=avg
-    return fmtDct,lst
+        valLst = dct[k]
+        avg = sum(valLst) / len(valLst)
+        fmtDct[k] = avg
+    return fmtDct, lst
 
 
-def write_st_e_s_cntnt(tup,row):
+def write_st_e_s_cntnt(tup, row):
     for srv in tup[1]:
-        row+=1
-        ha=srvHaDct.get(srv,'null')
-        az=hazDct.get(ha,'null')
-        preGen=ha[-5:]
-        preGen=re.sub('\d','',preGen)
-        genre=genreDct.get(preGen,'null')
-        val=tup[0][srv]
-        line=[srv,genre,ha,az,val] 
+        row += 1
+        ha = srvHaDct.get(srv, 'null')
+        az = hazDct.get(ha, 'null')
+        preGen = ha[-5:]
+        preGen = re.sub('\d', '', preGen)
+        genre = genreDct.get(preGen, 'null')
+        val = tup[0][srv]
+        line = [srv, genre, ha, az, val]
         if genre == 'null':
             notMatchLst.append(f'{sheetEchSrv}D{row}')
         if ha == 'null':
             notMatchLst.append(f'{sheetEchSrv}E{row}')
         if az == 'null':
             notMatchLst.append(f'{sheetEchSrv}F{row}')
-        sheetEchSrv.write_row(row,2,line,cfmt)
+        sheetEchSrv.write_row(row, 2, line, cfmt)
     return row
 
 
-def write_sheet_srv(tup3,tup4,tup5,cntntFunc,sheet,title):
-    sheet.write_row(0,0,title,cfmt)
-    r3=cntntFunc(tup3,0)
-    r4=cntntFunc(tup4,r3)
-    r5=cntntFunc(tup5,r4)
-    sheet.merge_range(1,1,r3,1,'可信3资源池',cfmt)
-    sheet.merge_range(r3+1,1,r4,1,'可信4资源池',cfmt)
-    sheet.merge_range(r4+1,1,r5,1,'可信5资源池',cfmt)
-    sheet.merge_range(1,0,r5,0,'西南大区',cfmt)
+def write_sheet_srv(tup3, tup4, tup5, cntntFunc, sheet, title):
+    sheet.write_row(0, 0, title, cfmt)
+    r3 = cntntFunc(tup3, 0)
+    r4 = cntntFunc(tup4, r3)
+    r5 = cntntFunc(tup5, r4)
+    sheet.merge_range(1, 1, r3, 1, '可信3资源池', cfmt)
+    sheet.merge_range(r3 + 1, 1, r4, 1, '可信4资源池', cfmt)
+    sheet.merge_range(r4 + 1, 1, r5, 1, '可信5资源池', cfmt)
+    sheet.merge_range(1, 0, r5, 0, '西南大区', cfmt)
 
 
 def crt_cls_srv_dct(tup):
-    dct={}
+    dct = {}
     for srv in tup[0]:
-        ha=srvHaDct.get(srv,'null')
-        preGen=ha[-5:]
-        preGen=re.sub('\d','',preGen)
-        genre=genreDct.get(preGen,'null')
-        dct.setdefault(genre,{})
-        dct[genre].setdefault(ha,[])
-        val=tup[0][srv]
+        ha = srvHaDct.get(srv, 'null')
+        preGen = ha[-5:]
+        preGen = re.sub('\d', '', preGen)
+        genre = genreDct.get(preGen, 'null')
+        dct.setdefault(genre, {})
+        dct[genre].setdefault(ha, [])
+        val = tup[0][srv]
         dct[genre][ha].append(val)
     return dct
 
 
 def sort_the_dct(dct):
-    lst=list(dct.keys())
+    lst = list(dct.keys())
     lst.sort()
     return lst
 
 
-def write_st_c_s_cntnt(tup,row):
-    dct=crt_cls_srv_dct(tup)
+def write_st_c_s_cntnt(tup, row):
+    dct = crt_cls_srv_dct(tup)
     for cls in sort_the_dct(dct):
-        sr=row+1
+        sr = row + 1
         for ha in sort_the_dct(dct[cls]):
-            row+=1
-            num=len(dct[cls][ha])
-            avg=sum(dct[cls][ha])/len(dct[cls][ha])
-            num=str(num)+'台'
-            az=hazDct.get(ha,'null')
-            line=[num,ha,az,avg]
-            sheetClsSrv.write_row(row,3,line,cfmt)
+            row += 1
+            num = len(dct[cls][ha])
+            avg = sum(dct[cls][ha]) / len(dct[cls][ha])
+            num = str(num) + '台'
+            az = hazDct.get(ha, 'null')
+            line = [num, ha, az, avg]
+            sheetClsSrv.write_row(row, 3, line, cfmt)
         if len(dct[cls]) == 1:
-            sheetClsSrv.write(row,2,cls,cfmt)
+            sheetClsSrv.write(row, 2, cls, cfmt)
         else:
-            sheetClsSrv.merge_range(sr,2,row,2,cls,cfmt)
+            sheetClsSrv.merge_range(sr, 2, row, 2, cls, cfmt)
     return row
 
 
 def write_srv_sheets():
-    tup3=fmt_the_dct('srvkx3.csv')
-    tup4=fmt_the_dct('srvkx4.csv')
-    tup5=fmt_the_dct('srvkx5.csv')
-    titleEch=['大区','资源池','服务器名称','物理机承载业务','所在HA','所在AZ','物理机CPU利用率']
-    titleCls=['大区','资源池','物理机承载业务','物理机台数','所在HA','所在AZ','各类物理机CPU利用率']
-    write_sheet_srv(tup3,tup4,tup5,write_st_e_s_cntnt,sheetEchSrv,titleEch)
-    write_sheet_srv(tup3,tup4,tup5,write_st_c_s_cntnt,sheetClsSrv,titleCls)
+    tup3 = fmt_the_dct('srvkx3.csv')
+    tup4 = fmt_the_dct('srvkx4.csv')
+    tup5 = fmt_the_dct('srvkx5.csv')
+    titleEch = ['大区', '资源池', '服务器名称', '物理机承载业务', '所在HA', '所在AZ', '物理机CPU利用率']
+    titleCls = ['大区', '资源池', '物理机承载业务', '物理机台数', '所在HA', '所在AZ', '各类物理机CPU利用率']
+    write_sheet_srv(tup3, tup4, tup5, write_st_e_s_cntnt, sheetEchSrv, titleEch)
+    write_sheet_srv(tup3, tup4, tup5, write_st_c_s_cntnt, sheetClsSrv, titleCls)
 
 
 def get_vnf(vmn):
-    dy=crt_dct('dy.csv')
-    others=crt_lst('vnf.csv')
+    dy = crt_dct('dy.csv')
+    others = crt_lst('vnf.csv')
     if "ZTE_EMSplus_RPT" in vmn:
         sbm = "ZTE_EMSplus_RPT"
     else:
@@ -191,27 +191,26 @@ def get_vnf(vmn):
     if vnfname == 'null':
         for i in others:
             if i in vmn:
-                vnfname=i
-        
+                vnfname = i
+
     return vnfname
 
 
 def write_sheet_ech_vm():
-    merge_files('vmkx3.csv','vmkx4.csv','vmtmp.csv',0) ##change the argument
-    merge_files('vmtmp.csv','vmkx5.csv','vm.csv',0)
-    title=['大区','资源池','网元名称','虚机名称','厂家','虚机vCPU 利用率']
-    sheetEchVM.write_row(0,0,title,cfmt)
-    dct=walk_the_file('vm.csv')
-    row=0
+    merge_files('vmkx3.csv', 'vmkx4.csv', 'vmtmp.csv', 0)  ##change the argument
+    merge_files('vmtmp.csv', 'vmkx5.csv', 'vm.csv', 0)
+    title = ['大区', '资源池', '网元名称', '虚机名称', '厂家', '虚机vCPU 利用率']
+    sheetEchVM.write_row(0, 0, title, cfmt)
+    dct = walk_the_file('vm.csv')
+    row = 0
     for vm in sort_the_dct(dct):
-        row+=1
-        vnf=get_vnf(vm)
-        avg=sum(dct[vm])/len(dct[vm])
-        line=['西南大区',f'可信{vm[12]}资源池',vnf,vm,'中兴',avg]
+        row += 1
+        vnf = get_vnf(vm)
+        avg = sum(dct[vm]) / len(dct[vm])
+        line = ['西南大区', f'可信{vm[12]}资源池', vnf, vm, '中兴', avg]
         if vnf == 'null':
             notMatchLst.append(f'{sheetEchVM}C{row}')
-        sheetEchVM.write_row(row,0,line,cfmt)
-
+        sheetEchVM.write_row(row, 0, line, cfmt)
 
 
 def do_it():
@@ -220,6 +219,7 @@ def do_it():
     write_srv_sheets()
     write_sheet_ech_vm()
     book.close()
+
 
 def crt_dct(file):
     dct = {}
@@ -232,7 +232,7 @@ def crt_dct(file):
 
 
 def crt_lst(file):
-    lst=[]
+    lst = []
     with open(file) as f:
         for l in f:
             lst.append(l.rstrip())
@@ -240,8 +240,10 @@ def crt_lst(file):
 
 
 def main():
-#    check_files()
+    #    check_files()
     do_it()
+
+
 #    fmt_it()
 #    echo_manual_op()
 
@@ -253,13 +255,13 @@ sheetClsSrv = book.add_worksheet('资源池各类物理机CPU利用率')
 sheetEchVM = book.add_worksheet('单虚机CPU利用率')
 sheetClsVM = book.add_worksheet('网元虚机CPU利用率')
 
-hazDct=crt_dct('haz.csv')
-srvHaDct=crt_dct('srvha.csv')
-genreDct={'--':'VIM服务器','MNG':'以虚机方式部署的管理域服务器','VIC':'业务域服务器','null':'null'}
+hazDct = crt_dct('haz.csv')
+srvHaDct = crt_dct('srvha.csv')
+genreDct = {'--': 'VIM服务器', 'MNG': '以虚机方式部署的管理域服务器', 'VIC': '业务域服务器', 'null': 'null'}
 
-notMatchLst=[]
+notMatchLst = []
 
-cfmt = book.add_format({'align': 'center', 'valign':'vcenter', 'border': 1})
+cfmt = book.add_format({'align': 'center', 'valign': 'vcenter', 'border': 1})
 
 if __name__ == '__main__':
     main()
