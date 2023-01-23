6a76b5967bfe54fa50d7b85a2b60a8c7d06f1db7
LiKai <likai945@foxmail.com>
2023-01-23 23:14:41 +0800

    pinshouqi_2•2023Jan23•23:14:41

---
+++ pinshouqi_2.py
@@ -0,0 +1,54 @@
+import random
+
+def split(money,quantity):
+    intM=float(money)*100
+    splQ=int(quantity)-1
+    lmtM=intM//int(quantity)
+    lowM=lmtM//3
+    lowM=1 if lowM==0 else lowM
+
+    if intM//(splQ+1)==0:
+        print("low to earth!")
+        return 2
+
+    for i in range(0,splQ):
+        oneM=random.randint(lowM,intM-splQ*lmtM)
+        spLst.append(oneM/100)
+        splQ-=1
+        intM-=oneM
+
+    spLst.append(intM/100)
+    random.shuffle(spLst)
+
+#    print(spLst)
+
+def lishi():
+    name=input("win:")
+    getM=spLst.pop()
+    if getM==luckyM:
+        luckyG.append(name)
+    print(f'\t{name} ¥{getM}')
+
+def main():
+    while True:
+        money=input('money:')
+        quantity=input('quantity:')
+        split(money,quantity)
+        if spLst:
+            break
+
+    global luckyM
+    luckyM=max(spLst)
+
+    while True:
+        lishi()
+        if not spLst:
+            print('\nno money')
+            print(f'lucky guy is {luckyG}, he got ¥{luckyM}.')
+            return
+
+spLst=[]
+luckyG=[]
+
+if __name__=='__main__':
+    main()
