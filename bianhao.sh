32927a05d751d59cf22901bfff4908accd6ff936
LiKai <likai945@foxmail.com>
2023-01-23 21:27:56 +0800

    bianhao•2023Jan23•21:27:56

---
+++ bianhao.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+>new_$1
+for i in `seq $(cat $1 | wc -l)`
+do
+	l=`sed -n "${i}p" $1`
+	if [[ $l =~ ^202.*日$ ]]
+	then
+		unset j
+		line=$l
+		echo $line >> new_$1
+	elif [[ $l =~ ^[[:space:]]*$ ]]
+	then
+		echo > /dev/null
+	else
+		let j++
+		line=$j、$l
+		echo $line >> new_$1
+	fi
+done
+sed -i 's/ //' new_$1
