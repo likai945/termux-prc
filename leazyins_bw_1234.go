f5555bdff1a486cbbec76c52cb8585b43b2bcc32
likai945 <likai945@foxmail.com>
2024-10-27 15:52:38 +0800

    first_go•2024Oct27•15:52:38

---
+++ leazyins_bw_1234.go
@@ -0,0 +1,114 @@
+//Oct 27, 2024
+//by LiKai
+
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+func getFile() string {
+	currentTime := time.Now()
+	fmtime := currentTime.Format("20060102")
+	pattern := fmt.Sprintf("%s_*.runlog", fmtime)
+	files, _ := filepath.Glob(pattern)
+	if len(files) == 0 {
+		fmt.Println("no todays runlog files")
+		os.Exit(2)
+	}
+	return files[0]
+}
+
+func readFile() [][]string {
+	fname := getFile()
+	fmt.Printf("\ncalculated %s\n", fname)
+	file, _ := os.Open(fname)
+	defer file.Close()
+
+	obj := csv.NewReader(file)
+	obj.Comma = '|'
+
+	var sli [][]string
+	for {
+		record, err := obj.Read()
+		if err == io.EOF {
+			break
+		}
+		sli = append(sli, record)
+	}
+	return sli[1:]
+}
+
+func trsDic() map[string][]int {
+	fobj := readFile()
+	res := map[string][]int{}
+	for _, i := range fobj {
+		device := i[0]
+		tempr := i[2]
+
+		devsli := []rune(device)
+		lendev := len(devsli)
+		num := string(devsli[11:14])
+		maj := string(devsli[27 : lendev-3])
+		//		num := string([]rune(device)[11:14])
+		//		maj := string([]rune(device)[27:30])
+		pool := num + "-" + maj
+		g_tempr, err := strconv.Atoi(tempr)
+
+		if err != nil {
+			continue
+		} else {
+			tmp := res[pool]
+			tmp = append(tmp, g_tempr)
+			res[pool] = tmp
+		}
+	}
+	return res
+}
+
+func compAvg(sli []int) float64 {
+	var sum int = 0
+	for _, i := range sli {
+		sum += i
+	}
+	addend := len(sli)
+	avg := float64(sum) / float64(addend)
+	return avg
+}
+
+func compute() map[string]float64 {
+	dict := trsDic()
+	computed := map[string]float64{}
+	for key, val := range dict {
+		avg := compAvg(val)
+		computed[key] = avg
+	}
+	return computed
+}
+
+func checkExis(res map[string]float64, key string) string {
+	val, exis := res[key]
+	if exis == true {
+		return fmt.Sprintf("%.2f°C", val)
+	} else {
+		return "no_data"
+	}
+}
+
+func main() {
+	res := compute()
+	fmt.Printf("\n===============\n")
+	fmt.Printf("03A-SRV/03A-DBS\t\t%s/%s\n", checkExis(res, "03A-SRV"), checkExis(res, "03A-DBS"))
+	fmt.Printf("04A-SRV/04A-DBS\t\t%s/%s\n", checkExis(res, "04A-SRV"), checkExis(res, "04A-DBS"))
+	fmt.Printf("05A-SRV/05A-DBS\t\t%s/%s\n", checkExis(res, "05A-SRV"), checkExis(res, "05A-DBS"))
+	fmt.Printf("09A-SRV/09A-DBS\t\t%s/%s\n", checkExis(res, "09A-SRV"), checkExis(res, "09A-DBS"))
+	fmt.Printf("11A-SRV/11A-DBS\t\t%s/%s\n", checkExis(res, "11A-HSRV"), checkExis(res, "11A-DBS"))
+	fmt.Printf("===============\n\n")
+
+}
--- xiao.py
+++ xiao.py
@@ -1,31 +1,32 @@
 import random
 import sys
 
+
 def select_one():
-    return random.randint(0,11)
+    return random.randint(0, 11)
 
 
 def select_five():
-    balls=list(range(1,49))
+    balls = list(range(1, 49))
     random.shuffle(balls)
-    zodiacs=[i%12 for i in balls]
+    zodiacs = [i % 12 for i in balls]
     return zodiacs[:5]
 
 
 def stir_balls():
-    balls=list(range(1,49))
+    balls = list(range(1, 49))
     random.shuffle(balls)
-    outbs=balls[:7]
+    outbs = balls[:7]
 
     return outbs
 
 
 def buy_mark():
-    one=select_one()
-    five=select_five()
-    balls=stir_balls()
-    zodiacs=[i%12 for i in balls]
-    print('-----')
+    one = select_one()
+    five = select_five()
+    balls = stir_balls()
+    zodiacs = [i % 12 for i in balls]
+    print("-----")
     print(zl[one])
     print([zl[i] for i in five])
     print(balls)
@@ -34,28 +35,27 @@ def buy_mark():
     global money
 
     if one in zodiacs:
-        money+=1
+        money += 1
     else:
-        money-=1
+        money -= 1
 
     for i in five:
         if i in zodiacs:
             zodiacs.remove(i)
-    if len(zodiacs)<3:
-        money+=100
-        print('win')
+    if len(zodiacs) < 3:
+        money += 100
+        print("win")
     else:
-        money-=1
+        money -= 1
 
     return money
 
 
-money=0
-zl=['🐍','🐲','🐰','🐯','🐮','🐀','🐷','🐶','🐔','🐒','🐑','🐴']
+money = 0
+zl = ["🐍", "🐲", "🐰", "🐯", "🐮", "🐀", "🐷", "🐶", "🐔", "🐒", "🐑", "🐴"]
 
-if __name__=='__main__':
+if __name__ == "__main__":
     for i in range(int(sys.argv[1])):
         buy_mark()
 
     print(money)
-
